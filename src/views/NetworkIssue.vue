97e676f8f27295ff190178689d57827d4fd1baa2
Chad Smith <smith.chad12@gmail.com>
2020-03-15 21:56:02 -0500

    add routes for error handling

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Uh-oh!</h1>
+    <h3>Looks like we are experiencing some issues. Please click the back button and try again.</h3>
+    <router-link :to="{name: 'event-list'}">Take me back home</router-link>
+  </div>
+</template>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Ooops!</h1>
+    <h3>The {{resource}} you're looking for was not found.</h3>
+    <router-link :to="{name: 'event-list'}">Take me back home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
